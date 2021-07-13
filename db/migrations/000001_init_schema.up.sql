a5d141cf0f60a20556b4fc8f107f30e09d74a8d2
Gregory Roberts <roberts.gregory@heb.com>
2021-07-13 17:07:26 -0500

    changed Customers to customers for table name

--- db/migrations/000001_init_schema.up.sql
+++ db/migrations/000001_init_schema.up.sql
@@ -1,5 +1,5 @@
 -- Creation of Customers table
-CREATE TABLE "Customers" (
+CREATE TABLE "customers" (
   "id" bigserial PRIMARY KEY,
   "first_name" varchar(50),
   "last_name" varchar(50),
@@ -10,18 +10,18 @@ CREATE TABLE "Customers" (
   "zip" varchar(5)
 );
 
-COMMENT ON COLUMN "Customers"."id" IS 'the primary key';
+COMMENT ON COLUMN "customers"."id" IS 'the primary key';
 
-COMMENT ON COLUMN "Customers"."first_name" IS '50 length becasue the longest out of millions was 46';
+COMMENT ON COLUMN "customers"."first_name" IS '50 length becasue the longest out of millions was 46';
 
-COMMENT ON COLUMN "Customers"."last_name" IS 'length similar to first name';
+COMMENT ON COLUMN "customers"."last_name" IS 'length similar to first name';
 
-COMMENT ON COLUMN "Customers"."email" IS 'Out of millions of customers, the longest was 64. This is to be safe';
+COMMENT ON COLUMN "customers"."email" IS 'Out of millions of customers, the longest was 64. This is to be safe';
 
-COMMENT ON COLUMN "Customers"."address" IS 'Longest in the united states is 95 characters long';
+COMMENT ON COLUMN "customers"."address" IS 'Longest in the united states is 95 characters long';
 
-COMMENT ON COLUMN "Customers"."city" IS 'The longest city name in the world is 85';
+COMMENT ON COLUMN "customers"."city" IS 'The longest city name in the world is 85';
 
-COMMENT ON COLUMN "Customers"."state" IS 'Minus Rhode island full state name, the longest is 13';
+COMMENT ON COLUMN "customers"."state" IS 'Minus Rhode island full state name, the longest is 13';
 
-COMMENT ON COLUMN "Customers"."zip" IS 'Only need 5. No ranges here just a single code';
+COMMENT ON COLUMN "customers"."zip" IS 'Only need 5. No ranges here just a single code';
